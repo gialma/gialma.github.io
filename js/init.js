fff6d06cf282ec50957376b6add4180d89389418
Alex Guerrieri <klaidliadon@gmail.com>
2017-07-12 13:07:05 +0200

    Async CSS minified

--- js/init.js
+++ js/init.js
@@ -13,7 +13,7 @@
 			// Global.
 				global: {
 					range: '*',
-					href: 'css/style.css',
+					href: 'css/style.min.css',
 					containers: 1400,
 					grid: {
 						gutters: {
@@ -26,14 +26,14 @@
 			// XLarge.
 				xlarge: {
 					range: '-1680',
-					href: 'css/style-xlarge.css',
+					href: 'css/style-xlarge.min.css',
 					containers: 1200
 				},
 
 			// Large.
 				large: {
 					range: '-1280',
-					href: 'css/style-large.css',
+					href: 'css/style-large.min.css',
 					containers: 960,
 					grid: {
 						gutters: {
@@ -48,7 +48,7 @@
 			// Medium.
 				medium: {
 					range: '-980',
-					href: 'css/style-medium.css',
+					href: 'css/style-medium.min.css',
 					containers: '90%',
 					grid: {
 						collapse: 1
@@ -58,7 +58,7 @@
 			// Small.
 				small: {
 					range: '-736',
-					href: 'css/style-small.css',
+					href: 'css/style-small.min.css',
 					containers: '90%',
 					grid: {
 						gutters: {
@@ -70,7 +70,7 @@
 			// XSmall.
 				xsmall: {
 					range: '-480',
-					href: 'css/style-xsmall.css',
+					href: 'css/style-xsmall.min.css',
 					grid: {
 						collapse: 2
 					}
